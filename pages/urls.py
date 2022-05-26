20dfc4c5c0cb49f7b7f0272e2503f5e729a89fa2
lodyne <lodgmtui@gmail.com>
2022-05-26 17:16:43 +0300

    make urls and views

--- pages/urls.py
+++ pages/urls.py
@@ -1,8 +1,17 @@
 from django.urls import path
 
-from .views import HomePageView, AboutPageView
+from .views import (
+    DiscussionPageView,
+    ForumPageView, 
+    HomePageView, 
+    AboutPageView, 
+    SkillsPageView,
+)
 
 urlpatterns = [
     path("", HomePageView.as_view(), name="home"),
     path("about/", AboutPageView.as_view(), name="about"),
+    path("forum/", ForumPageView.as_view(), name="forum"),
+    path("discussion/", DiscussionPageView.as_view(), name="discussion"),
+    path("skill/", SkillsPageView.as_view(), name="skill"),
 ]
--- pages/views.py
+++ pages/views.py
@@ -4,6 +4,14 @@ from django.views.generic import TemplateView
 class HomePageView(TemplateView):
     template_name = "pages/home.html"
 
-
 class AboutPageView(TemplateView):
     template_name = "pages/about.html"
+
+class ForumPageView(TemplateView):
+    template_name = "pages/forum.html"
+
+class DiscussionPageView(TemplateView):
+    template_name = "pages/discussion.html"
+
+class SkillsPageView(TemplateView):
+    template_name = "pages/skills.html"
